af32d470bcbc9a75a02babc6e85ed9c0cee113d7
Jose Espinosa <jhespinosa@gmail.com>
2021-09-08 12:02:17 -0700

    Consolidate the python scripts to one

--- python/getQuotes.py
+++ python/getQuotes.py
@@ -4,16 +4,19 @@ import urllib3
 from bs4 import BeautifulSoup
 import sys
 
-class BrainyQuote:
+selector = {"www.goodreads.com": ".quoteText", "www.brainyquote.com": ".b-qt"}
+
+class GetQuotes:
 	def __init__(self):
+		host = urllib3.get_host(sys.argv[1])[1]
+		self.cssSel = selector[host]
 		self.soup = BeautifulSoup(urllib3.PoolManager().request("GET", sys.argv[1]).data.decode("utf-8"), "html.parser")
 		super().__init__()
 
-
 	def process(self):
-		for a in self.soup.select(".b-qt"):
+		for a in self.soup.select(self.cssSel):
 			print(a.get_text())
 			print("--")
 
-BrainyQuote().process()
+GetQuotes().process()
 
