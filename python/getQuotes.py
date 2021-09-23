4d7f4d6812630ff9891a796af4677ed9246b58ba
Jose Espinosa <jhespinosa@gmail.com>
2021-09-23 14:06:03 -0700

    Add www.ascensiongateway.com to quote script. Handle whenno selector is found

--- python/getQuotes.py
+++ python/getQuotes.py
@@ -15,26 +15,37 @@ selector = {
 	"citatis.com": ["blockquote"],
 	"quotestats.com": ["blockquote"],
         "upjourney.com": ["h2~p > em"],
+	"www.ascensiongateway.com": ["font > ul > li > font"],
+}
+
+remove = {
+	"www.ascensiongateway.com": ["a"],
 }
 
 class GetQuotes:
 	def __init__(self):
 		host = urllib3.get_host(sys.argv[1])[1]
-		self.cssSel = selector[host]
+		self.cssSel = selector.get(host)
+		self.cssSelRem = selector.get(host)
 		self.soup = BeautifulSoup(urllib3.PoolManager().request("GET", sys.argv[1]).data.decode("utf-8"), "html.parser")
 		super().__init__()
 
 	def process(self):
-		for s in self.cssSel:
-			for a in self.soup.select(s):
-				line  = a.get_text()
-				if line:
-					line = re.sub("\n+","\n",line)
-					line = re.sub("–.*|”|“|$#?[0-9]+\\.\\s","",line)
-					line = re.sub("^\s+", "", line)
-					line = re.sub("\s+$", "", line)
-					print(line)
-					print("--")
+		if self.cssSel:
+			for s in self.cssSel:
+				for a in self.soup.select(s):
+					if self.cssSelRem:
+						for r in self.cssSelRem:
+							for rr in a.findAll(r):
+								rr.decompose()
+					line  = a.get_text()
+					if line:
+						line = re.sub("\s+"," ",line)
+						line = re.sub("–.*|”|“|$#?[0-9]+\\.\\s+|-","",line)
+						line = re.sub("[\s|\n]+$", "\n", line)
+
+						print(line)
+						print("--")
 
 GetQuotes().process()
 
